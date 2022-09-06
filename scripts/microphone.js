df20ff699d5b2d1d26886c1be6c092298548e249
Aremu Smog <aremuoluwagbamila@gmail.com>
2022-09-06 21:28:30 +0100

    ao_feat: make things work

--- scripts/microphone.js
+++ scripts/microphone.js
@@ -2,15 +2,16 @@ import { checkIfUserAuthenticated, signUserInAndOut } from "./authentication"
 import { getCounter } from "./firebase"
 
 let offTheGrid = setInterval(function () {
-  if (document.readyState !== "complete") return
-  clearInterval(offTheGrid)
+	if (document.readyState !== "complete") return
+	clearInterval(offTheGrid)
 
-  getCounter()
-  checkIfUserAuthenticated()
+	getCounter()
+	checkIfUserAuthenticated()
 }, 100)
 
 const signInButton = document.querySelector(".sign-in")
 signInButton.addEventListener("click", e => {
-  e.preventDefault()
-  signUserInAndOut()
+	e.preventDefault()
+	alert("Sign me in!")
+	signUserInAndOut()
 })
--- scripts/speech-recongition.js
+++ scripts/speech-recongition.js
@@ -1,12 +1,16 @@
 import { updateCounter } from "./firebase"
 import { startChanting, stopChanting, comeToLife } from "./animation"
 
-const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
-const SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
-const SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
+var SpeechRecognitionEvent =
+	SpeechRecognitionEvent || webkitSpeechRecognitionEvent
 
 var donda_list = ["thunder", "dun da", "dunda", "donda"]
-var grammar = "#JSGF V1.0; grammar donda_list; public <donda> = " + donda_list.join(" | ") + " ;"
+var grammar =
+	"#JSGF V1.0; grammar donda_list; public <donda> = " +
+	donda_list.join(" | ") +
+	" ;"
 
 var recognition = new SpeechRecognition()
 var speechRecognitionList = new SpeechGrammarList()
@@ -18,59 +22,61 @@ recognition.interimResults = false
 recognition.maxAlternatives = 1
 
 export const praiseGod = () => {
-  const jail = document.querySelector(".jail")
-  jail.addEventListener("click", () => {
-    recognition.start()
+	const jail = document.querySelector(".jail")
+	jail.addEventListener("click", () => {
+		recognition.start()
 
-    comeToLife(1)
+		comeToLife(1)
 
-    console.log("Start chanting")
-  })
+		console.log("Start chanting")
+	})
 }
 
 recognition.onresult = function (event) {
-  const dondaOptions = new RegExp(donda_list.join("|"), "g")
+	const dondaOptions = new RegExp(donda_list.join("|"), "g")
 
-  const resultFromSpeech = event.results[0][0].transcript
+	const resultFromSpeech = event.results[0][0].transcript
 
-  const dondaMentions = resultFromSpeech.match(dondaOptions)
+	const dondaMentions = resultFromSpeech.match(dondaOptions)
 
-  if (dondaMentions) {
-    const noOfDondaMentions = dondaMentions.length || 0
+	if (dondaMentions) {
+		const noOfDondaMentions = dondaMentions.length || 0
 
-    console.log(noOfDondaMentions)
-    updateCounter(noOfDondaMentions)
-  } else {
-    console.log("Result not matched")
-  }
+		console.log(noOfDondaMentions)
+		updateCounter(noOfDondaMentions)
+	} else {
+		console.log("Result not matched")
+	}
 }
 
 const BREAK_TIME = 2000
 
 recognition.onsoundstart = function () {
-  startChanting()
+	startChanting()
 }
 recognition.onspeechend = function () {
-  recognition.stop()
-  comeToLife(0.3)
-  stopChanting()
-  console.log(`Take a deep breath and get back to it in ${BREAK_TIME} milliseconds`)
+	recognition.stop()
+	comeToLife(0.3)
+	stopChanting()
+	console.log(
+		`Take a deep breath and get back to it in ${BREAK_TIME} milliseconds`
+	)
 
-  setTimeout(() => {
-    console.log("Keep going")
-    recognition.start()
-    comeToLife(1)
-  }, BREAK_TIME)
+	setTimeout(() => {
+		console.log("Keep going")
+		recognition.start()
+		comeToLife(1)
+	}, BREAK_TIME)
 }
 
 recognition.onnomatch = function (event) {
-  stopChanting()
-  comeToLife(0.3)
-  console.log("Not Donda")
+	stopChanting()
+	comeToLife(0.3)
+	console.log("Not Donda")
 }
 
 recognition.onerror = function (event) {
-  comeToLife(0.3)
-  stopChanting()
-  console.log(event.error)
+	comeToLife(0.3)
+	stopChanting()
+	console.log(event.error)
 }
