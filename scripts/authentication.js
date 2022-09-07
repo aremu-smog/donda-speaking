727d9f2dfee9b1296c90576cb8c56aeb3a4877e5
Aremu Smog <aremuoluwagbamila@gmail.com>
2022-09-07 23:25:14 +0100

    chore: move sensitive data to environment variables

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -1,4 +1,9 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth"
+import {
+	getAuth,
+	signInWithPopup,
+	GoogleAuthProvider,
+	signOut,
+} from "firebase/auth"
 import { startFirebase } from "./firebase"
 import { praiseGod } from "./speech-recongition"
 
@@ -8,62 +13,62 @@ const auth = getAuth()
 
 const signInButton = document.querySelector(".sign-in")
 export const checkIfUserAuthenticated = () => {
-  const user = auth.currentUser
+	const user = auth.currentUser
 
-  if (user) {
-    if (user.email === "aremuoluwagbamila@gmail.com") {
-      praiseGod()
-      signInButton.innerText = user.displayName
-    } else {
-      window.location.href = "index.html"
-    }
-  } else {
-  }
+	if (user) {
+		if (user.email === process.env.AUTHORIZED_USER) {
+			praiseGod()
+			signInButton.innerText = user.displayName
+		} else {
+			window.location.href = "index.html"
+		}
+	} else {
+	}
 }
 
 export const signIn = () => {
-  const provider = new GoogleAuthProvider()
-  provider.setCustomParameters({
-    prompt: "select_account"
-  })
+	const provider = new GoogleAuthProvider()
+	provider.setCustomParameters({
+		prompt: "select_account",
+	})
 
-  signInWithPopup(auth, provider)
-    .then(result => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result)
-      const token = credential.accessToken
-      // The signed-in user info.
-      const user = result.user
+	signInWithPopup(auth, provider)
+		.then(result => {
+			// This gives you a Google Access Token. You can use it to access the Google API.
+			const credential = GoogleAuthProvider.credentialFromResult(result)
+			const token = credential.accessToken
+			// The signed-in user info.
+			const user = result.user
 
-      checkIfUserAuthenticated()
-    })
-    .catch(error => {
-      // Handle Errors here.
-      const errorCode = error.code
-      const errorMessage = error.message
-      // The email of the user's account used.
-      const email = error.email
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error)
-      // ...
-    })
+			checkIfUserAuthenticated()
+		})
+		.catch(error => {
+			// Handle Errors here.
+			const errorCode = error.code
+			const errorMessage = error.message
+			// The email of the user's account used.
+			const email = error.email
+			// The AuthCredential type that was used.
+			const credential = GoogleAuthProvider.credentialFromError(error)
+			// ...
+		})
 }
 
 export const signUserOut = () => {
-  signOut(auth)
-    .then(() => {
-      signInButton.innerText = "Sign in"
-    })
-    .catch(error => {
-      aler(error.message)
-    })
+	signOut(auth)
+		.then(() => {
+			signInButton.innerText = "Sign in"
+		})
+		.catch(error => {
+			aler(error.message)
+		})
 }
 
 export const signUserInAndOut = () => {
-  const user = auth.currentUser
-  if (user) {
-    signUserOut()
-  } else {
-    signIn()
-  }
+	const user = auth.currentUser
+	if (user) {
+		signUserOut()
+	} else {
+		signIn()
+	}
 }
--- scripts/firebase.js
+++ scripts/firebase.js
@@ -3,22 +3,22 @@ import { initializeApp } from "firebase/app"
 import { getDatabase, ref, onValue, increment, update } from "firebase/database"
 
 const dotSeparation = number => {
-  const commaSeparated = number.toLocaleString()
+	const commaSeparated = number.toLocaleString()
 
-  const dotSeperated = commaSeparated.replace(",", ".")
+	const dotSeperated = commaSeparated.replace(",", ".")
 
-  return dotSeperated
+	return dotSeperated
 }
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBVfCwskoFtmkUfydbPJFaYmPYntn8hbP4",
-  authDomain: "donda-project-dev.firebaseapp.com",
-  databaseURL: "https://donda-project-dev-default-rtdb.firebaseio.com",
-  projectId: "donda-project-dev",
-  storageBucket: "donda-project-dev.appspot.com",
-  messagingSenderId: "80315066249",
-  appId: "1:80315066249:web:75f81031ce8466753d6358",
-  measurementId: "G-4XS60Z3HQB"
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.FIREBASE_DATABASE_URL,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
+	appId: process.env.FIREBASE_APP_ID,
+	measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
@@ -30,17 +30,17 @@ const db = getDatabase()
 const dbRef = ref(db)
 const dondaCounter = ref(db, "counter")
 export const getCounter = () =>
-  onValue(dondaCounter, snapshot => {
-    const data = snapshot.val()
+	onValue(dondaCounter, snapshot => {
+		const data = snapshot.val()
 
-    const counterTag = document.querySelector(".counter")
+		const counterTag = document.querySelector(".counter")
 
-    counterTag.innerText = dotSeparation(data)
-  })
+		counterTag.innerText = dotSeparation(data)
+	})
 
 export const updateCounter = amount => {
-  const updates = {}
-  updates["counter"] = increment(amount)
+	const updates = {}
+	updates["counter"] = increment(amount)
 
-  update(dbRef, updates)
+	update(dbRef, updates)
 }
